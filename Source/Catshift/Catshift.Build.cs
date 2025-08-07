a600819be22fc758184f381285f9516abbd69db1
Valentina Restrepo <valentina.re.san@gmail.com>
2025-08-07 13:40:49 -0500

    Repository created

---
+++ Source/Catshift/Catshift.Build.cs
@@ -0,0 +1,43 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Catshift : ModuleRules
+{
+	public Catshift(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"Catshift",
+			"Catshift/Variant_Platforming",
+			"Catshift/Variant_Combat",
+			"Catshift/Variant_Combat/AI",
+			"Catshift/Variant_SideScrolling",
+			"Catshift/Variant_SideScrolling/Gameplay",
+			"Catshift/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/Catshift/CatshiftGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CatshiftGameMode.h"
+
+ACatshiftGameMode::ACatshiftGameMode()
+{
+	// stub
+}
