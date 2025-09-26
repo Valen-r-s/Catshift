971d8219cb913741db53560b23f718acea2ac9d9
Juandejdoc <est.juand.orozco@unimilitar.edu.co>
2025-09-25 20:38:40 -0500

    Bug fix and Clothes simulation

---
+++ Source/Catshift/TopDownController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TopDownController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CATSHIFT_API ATopDownController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
